a2c23e016b66747eed3c57c8845c273a6b724ceb
Ouattara Junior <ouattarajunior418@gmail.com>
2024-10-27 10:18:35 +0000

    retirer les clés

--- building_directory/settings.py
+++ building_directory/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from dotenv import load_dotenv
+import os
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9s*9xpsckn-g-01#xi40_%!)p9tz6wkzs^7kjz^q8py=*zm_=8'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -168,10 +172,11 @@ SOCIALACCOUNT_PROVIDERS = {
             'access_type': 'online',
         },
         'METHOD': 'oauth2',
-        'CLIENT_ID': '169461568516-2j6as3oc806l2bt2pu0ojcjif8tsdvkp.apps.googleusercontent.com',
-        'SECRET': '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',
+        'CLIENT_ID': os.getenv("GOOGLE_CLIENT_ID"),
+        'SECRET': os.getenv("GOOGLE_CLIENT_SECRET"),
     }
 }
+
 
 
 # URLs de redirection après authentification
